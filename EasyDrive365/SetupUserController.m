da8c5dde130ff46d6b15bd57d8ccd4a9b55a7b01
Steven Fu <fuxinyu@gmail.com>
2014-01-26 16:55:31 +0800

    fix upload image size

--- EasyDrive365/SetupUserController.m
+++ EasyDrive365/SetupUserController.m
@@ -256,6 +256,42 @@
     UIGraphicsEndImageContext();
     return scaledImage;
 }
+- (UIImage *)scaleImage:(UIImage *) image maxWidth:(float) maxWidth maxHeight:(float) maxHeight
+{
+    CGImageRef imgRef = image.CGImage;
+    CGFloat width = CGImageGetWidth(imgRef);
+    CGFloat height = CGImageGetHeight(imgRef);
+    if (width <= maxWidth && height <= maxHeight)
+    {
+        return image;
+    }
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    CGRect bounds = CGRectMake(0, 0, width, height);
+    if (width > maxWidth || height > maxHeight)
+    {
+        CGFloat ratio = width/height;
+        if (ratio > 1)
+        {
+            bounds.size.width = maxWidth;
+            bounds.size.height = bounds.size.width / ratio;
+        }
+        else
+        {
+            bounds.size.height = maxHeight;
+            bounds.size.width = bounds.size.height * ratio;
+        }
+    }
+    CGFloat scaleRatio = bounds.size.width / width;
+    UIGraphicsBeginImageContext(bounds.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextScaleCTM(context, scaleRatio, -scaleRatio);
+    CGContextTranslateCTM(context, 0, -height);
+    CGContextConcatCTM(context, transform);
+    CGContextDrawImage(UIGraphicsGetCurrentContext(), CGRectMake(0, 0, width, height), imgRef);
+    UIImage *imageCopy = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return imageCopy;
+}
 - (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo;
 {
     if (!error){
@@ -272,7 +308,8 @@
     _image =[info valueForKey:UIImagePickerControllerOriginalImage];
     [self dismissViewControllerAnimated:YES completion:nil];
     
-    [self saveImage:[self scaleToSize:_image size:CGSizeMake(600, 600)]];
+    //[self saveImage:[self scaleToSize:_image size:CGSizeMake(600, 600)]];
+    [self saveImage:[self scaleImage:_image maxWidth:640 maxHeight:960]];
 }
 
 - (void) pickImage:(UIImagePickerControllerSourceType )sourceType
